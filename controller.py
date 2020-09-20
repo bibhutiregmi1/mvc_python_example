d12d8cf0a53e7d015f9e405b4d4c8bd2796539b2
bibhutiregmi1 <bibhutiregmi1@gmail.com>
2020-09-20 23:09:31 +0545

    template added

--- controller.py
+++ controller.py
@@ -1,13 +1,15 @@
 from model.student import Student
 import view.viewStudent as view
-
+from flask import Flask
+app = Flask(__name__)
 
 def showAll():
     student_in_db = Student.getAll()
     return view.showAllView(student_in_db)
 
+@app.route('/')
 def start():
     return showAll()
 
 if __name__=='__main__':
-    start()
+    app.run()
--- view/viewStudent.py
+++ view/viewStudent.py
@@ -1,5 +1,7 @@
 from model.student import Student
+from flask import  render_template
 
 def showAllView(list):
     for item in list:
         print (item.studentInfo())
+    return render_template('index.html', list= list)
